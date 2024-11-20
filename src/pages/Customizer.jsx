25cb916fd413972af78fe9feb7715f270e2f2a33
Muhammad Abdullah <mabdullah@ogoul.com>
2024-11-21 01:34:53 +0500

    customizer component started

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,4 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+import config from "../config/config"
+import state from "../store/index"
+import { download } from "../assets/download.png"
+import { downloadCanvasToImage, reader } from '../config/helpers'
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants'
+import { fadeAnimation, slideAnimation } from "../config/motion"
+import AIpicker from '../components/AIpicker'
+import ColorPicker from '../components/ColorPicker'
+import FilePicker from '../components/FilePicker'
+import Tab from '../components/Tab'
 
 const Customizer = () => {
   return (
