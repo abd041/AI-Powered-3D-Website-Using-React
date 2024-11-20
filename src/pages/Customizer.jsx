4847a855e58072121a48832049e100b7497e03b7
Muhammad Abdullah <mabdullah@ogoul.com>
2024-11-21 01:58:36 +0500

    customizer structure is ready

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -11,10 +11,50 @@ import AIpicker from '../components/AIpicker'
 import ColorPicker from '../components/ColorPicker'
 import FilePicker from '../components/FilePicker'
 import Tab from '../components/Tab'
+import Custombtn from "../components/Custombtn"
 
 const Customizer = () => {
+  const snap = useSnapshot(state)
   return (
-    <div>Customizer</div>
+    <AnimatePresence>
+      {!snap.intro && <>
+        <motion.div {...slideAnimation("left")} className='absolute top-0 left-0 z-10'>
+          <div className='flex items-center min-h-screen'>
+            <div className='editorstabs-container tabs'>
+              {EditorTabs.map((tab) => (
+                <Tab
+                  key={tab.name}
+                  tab={tab}
+                  handleClick={() => { }}
+                />
+              ))}
+
+            </div>
+          </div>
+
+        </motion.div>
+        <motion.div className='absolute right-4 top-4 z-10' {...fadeAnimation}>
+          <Custombtn
+            type="filled"
+            title="Go back"
+            handleClick={() => state.intro = true}
+            customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+          />
+        </motion.div>
+        <motion.div className='filtertabs-container' {...slideAnimation("up")}>
+          {FilterTabs.map((tab) => (
+            <Tab
+              isFilterTab=""
+              isActiveTab=""
+              key={tab.name}
+              tab={tab}
+              handleClick={() => { }}
+            />
+          ))}
+        </motion.div>
+      </>}
+    </AnimatePresence>
+
   )
 }
 
